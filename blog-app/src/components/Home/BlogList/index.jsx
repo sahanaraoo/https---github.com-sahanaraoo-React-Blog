84b36eda7caab234e3c8670427787a6e6bbcef43
sahanaraoo <sahana2003.rao@gmail.com>
2023-12-06 21:00:42 +0530

    new commit

--- blog-app/src/components/Home/BlogList/index.jsx
+++ blog-app/src/components/Home/BlogList/index.jsx
@@ -7,14 +7,14 @@ const BlogList = ({ blogs }) => {
     <div className="blogList-wrap">
       {blogs.map((blog) => (
         <div key={blog.id} className="blog-item">
-          {/* Check if cover exists before rendering the image */}
+          
           {blog.cover && (
             <img src={blog.cover.file.url} alt={blog.title} />
           )}
 
           <h3>{blog.title}</h3>
 
-          {/* Truncate the description to show only 10 lines */}
+          
           <p>{truncateDescription(blog.description, 10)}</p>
 
           <p>{`Author: ${blog.authorName}`}</p>
@@ -30,7 +30,7 @@ const BlogList = ({ blogs }) => {
   );
 };
 
-// Truncate description function
+
 const truncateDescription = (description, maxLines) => {
   if (!description) {
     return '';
