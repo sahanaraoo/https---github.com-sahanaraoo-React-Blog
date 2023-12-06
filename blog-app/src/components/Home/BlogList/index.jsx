667ebd3844e3c38f67f86c18382261bc30f89f70
sahanaraoo <sahana2003.rao@gmail.com>
2023-12-06 20:53:16 +0530

    new commit

--- blog-app/src/components/Home/BlogList/index.jsx
+++ blog-app/src/components/Home/BlogList/index.jsx
@@ -3,22 +3,24 @@ import { Link } from 'react-router-dom';
 import './style.css';
 
 const BlogList = ({ blogs }) => {
-  const truncateDescription = (description, maxLines) => {
-    const lines = description.split('\n');
-    const truncated = lines.slice(0, maxLines).join('\n');
-    return truncated;
-  };
-
   return (
     <div className="blogList-wrap">
       {blogs.map((blog) => (
         <div key={blog.id} className="blog-item">
-          <img src={blog.cover} alt={blog.title} />
+          {/* Check if cover exists before rendering the image */}
+          {blog.cover && (
+            <img src={blog.cover.file.url} alt={blog.title} />
+          )}
+
           <h3>{blog.title}</h3>
-          <p className="truncated-description">{truncateDescription(blog.description, 10)}</p>
+
+          {/* Truncate the description to show only 10 lines */}
+          <p>{truncateDescription(blog.description, 10)}</p>
+
           <p>{`Author: ${blog.authorName}`}</p>
           <p>{`Category: ${blog.category}`}</p>
           <p>{`Created At: ${blog.createdAt}`}</p>
+
           <Link to={`/blog/${blog.id}`} className="read-more-link">
             Read More
           </Link>
@@ -26,6 +28,17 @@ const BlogList = ({ blogs }) => {
       ))}
     </div>
   );
+};
+
+// Truncate description function
+const truncateDescription = (description, maxLines) => {
+  if (!description) {
+    return '';
+  }
+
+  const lines = description.split('\n');
+  const truncatedLines = lines.slice(0, maxLines);
+  return truncatedLines.join('\n');
 };
 
 export default BlogList;
